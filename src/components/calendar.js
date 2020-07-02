72a84d6dac74ee6e96d73eec3de6fa5694443bec
Dilum1999 <45029973+Dilum1999@users.noreply.github.com>
2020-07-02 16:19:45 -0700

    Created the two function and buttons but the buttons are not working

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
+import './calendar.css';
 
 const Calendar = () => {
 	let today = new Date();
 	let currentYear = today.getFullYear();
 	let currentMonth = today.getMonth();
-	let day = today.getDay();
+	// let day = today.getDay();
 
 	const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 	];
 
-	let monthName = monthNames[currentMonth];
 	
+	const next = () => {
+		currentYear = (currentYear === 11) ? currentYear + 1 : currentYear;
+		currentMonth = (currentMonth + 1) % 12;
+		generateRows(currentMonth,currentYear)
+	};
+
+	const previous = () => {
+		currentYear = (currentYear === 0) ? currentYear - 1 : currentYear;
+		currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+		generateRows(currentMonth,currentYear)
+	}
+
 
 	const generateRows = (month, year) => {
 		let firstDay = new Date(year, month, 1).getDay();
@@ -22,6 +34,7 @@ const Calendar = () => {
 		//Clearing the tabel
 		let tbody = [];
 		let date = 1;
+		let keyVal = 1;
 		
 		// Outer for loop is for creating colums
 		for(let i = 0; i < 6; i++){
@@ -32,29 +45,30 @@ const Calendar = () => {
 			for(let j = 0; j < 7; j++){
 				// The first column of calendar
 				if(i === 0 && j < firstDay) {
-					let cell = React.createElement('td', {}, "")
+					let cell = React.createElement('td', {key : keyVal}, "")
 					data.push(cell)
 				}else if(date > numberOfDays){
 						break;
 				}
 				else {
-					let cell = React.createElement('td', {}, date)
+					let cell = React.createElement('td', {key : keyVal}, date)
 					data.push(cell)
 					date++;
 				}
+				keyVal ++;
 			}
 			//create a tabel row
-			let row = React.createElement('tr', {}, data)
+			let row = React.createElement('tr', {key:i}, data)
 			tbody.push(row) //appending all the rows innto the calendar body
 			data = [];// Removing the previous cells after adding them to the table body
 		}
 		return React.createElement('tbody', {}, tbody)
 	}
-	
+	let monthName = monthNames[currentMonth];
 
 	return(
 		<div>
-		<h1>{currentYear, monthName}</h1>
+		<h1>{ monthName}</h1>
 			<Table >
 				<thead>
 					<tr>
@@ -69,6 +83,10 @@ const Calendar = () => {
 				</thead>
 				{generateRows(currentMonth,currentYear)}
 			</Table>
+			<div className="btns"> 
+				<button onClick={previous}>Previous</button>
+				<button onClick={next}>Next</button>
+			</div>
 		</div>
 	);
 };
