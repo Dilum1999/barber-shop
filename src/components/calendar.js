4011cb302bc9cf14e0cfa1021b1ad2214f853800
Dilum1999 <45029973+Dilum1999@users.noreply.github.com>
2020-07-01 23:16:29 -0700

    Added some basic Date objects to find the current dates and so on

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -2,9 +2,27 @@ import React from 'react';
 import Table from 'react-bootstrap/Table';
 
 const Calendar = () => {
+	let today = new Date();
+	let year = today.getFullYear();
+	let month = today.getMonth();
+	let day = today.getDay();
+
+	const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+	];
+
+	let monthName = monthNames[month];
+
+	const generateRows = () => {
+		const firstDay = new Date(year, month, 1)
+		const endDay = new Date(year,month + 1, 0)
+		return endDay.getDay() 
+	}
+
 	return(
 		<div>
-		<h1>Hello</h1>
+		<h1>{year}</h1>
+		<h1>{generateRows()}</h1>
 			<Table >
 				<thead>
 					<tr>
