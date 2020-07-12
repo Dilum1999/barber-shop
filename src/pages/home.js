76c6f73b3f11f0978aa5a868fd6a78bf2a50a266
Dilum1999 <45029973+Dilum1999@users.noreply.github.com>
2020-07-12 15:19:18 -0700

    Changed the layout of the the page

--- src/pages/home.js
+++ src/pages/home.js
@@ -25,7 +25,24 @@ function Home() {
 	return(
 		<div>
 			<ControlledCarousel />
-			<CardImage/>
+				<Container className="py-5" >
+					<h4><Image src={alert} 
+					style={{width:'30px', height:'auto', margin: '10px'}}/>
+					 COVID-19 SAFETY MEASURES</h4>
+					<p className='font-weight-bold'>In order to ensure a safe ennvironment for all our custormers
+					we have some few safety measures that we would like to insist you on.</p>
+					<ul className='pl-5'>
+						<li>Please be on time for your appointment as we are trying to 
+							limit the number of people in our shop at all times.</li>
+						<li>
+							we would encourage you to wear a mask if possible 
+						</li>
+						<li>
+							We have hand sanitizers in the shop for you to use.
+						</li>
+					</ul>
+				</Container>
+				<CardImage/>
 			<Container className="text-center py-4">
 				<h4>Our Services</h4>
 				<hr />
@@ -57,24 +74,7 @@ function Home() {
 						</Col>
 					</Row>
 				</Container>
-				<hr/>
-				<Container >
-					<h4><Image src={alert} 
-					style={{width:'30px', height:'auto', margin: '10px'}}/>
-					 COVID-19 SAFETY MEASURES</h4>
-					<p className='font-weight-bold'>In order to ensure a safe ennvironment for all our custormers
-					we have some few safety measures that we would like to insist you on.</p>
-					<ul className='pl-5'>
-						<li>Please be on time for your appointment as we are trying to 
-							limit the number of people in our shop at all times.</li>
-						<li>
-							we would encourage you to wear a mask if possible 
-						</li>
-						<li>
-							We have hand sanitizers in the shop for you to use.
-						</li>
-					</ul>
-				</Container>
+				
 		</div>
 	);
 }
